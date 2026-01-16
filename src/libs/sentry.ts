777dae5ae984cb1f473724d69a0944f3ab52540c
Davyd Kewen <davydkewen@gmail.com>
2026-01-16 11:30:07 -0300

    feat: add new sentry config

--- src/libs/sentry.ts
+++ src/libs/sentry.ts
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/node";
 
 Sentry.init({
-  dsn: "https://9766369ce1ce66e2f7bb47ad6d4fc8f5@o4507509902606336.ingest.us.sentry.io/4509924424482816",
+  dsn: "https://f77c2884f37e3295d775b9696786bbeb@o4507509902606336.ingest.us.sentry.io/4510720568721408",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
-  enabled: true,
 });
+
